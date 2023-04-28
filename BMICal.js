c8542729516793090c4b9a5d0533198929c77b70
RainyF0rest <107770951+RainyF0rest@users.noreply.github.com>
2023-04-28 23:47:33 +0800

    butto

--- BMICal.js
+++ BMICal.js
@@ -8,11 +8,12 @@ function bmiCal()
     const medium = '<iframe src="https://giphy.com/embed/bDL3BsB4ViI2k" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>↑You are healthy and fit ! ↑</p>'
     const chubby = '<iframe src="https://giphy.com/embed/47EstTgsR1vpK" width="480" height="369" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>↑Bit of chubby ! Still fine↑</p>'
     const plump = '<iframe src="https://giphy.com/embed/lN9amhr8GZMhG" width="480" height="419" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>↑Overweight ! Start your diet or you may suffer with health issue !↑</p>'
+    const alien = '<iframe src="https://giphy.com/embed/PApUlVfEFmZAQ" width="272" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>↑Are you really a human being ? ↑</p>'
     const bmi = w/((h*h));
     console.log(`BMI : ${bmi}`);
     document.getElementById("BMI").value = bmi;
 
-    if( bmi < 18.5)
+    if( bmi < 18.5 && bmi >= 10 )
     {
         document.getElementById("result").innerHTML = thin ;
     }
@@ -24,13 +25,13 @@ function bmiCal()
     {
         document.getElementById("result").innerHTML = chubby ;
     }
-    else if( bmi >=27 )
+    else if( bmi >=27 && bmi <= 40 )
     {
         document.getElementById("result").innerHTML = plump ;
     }
-    else if ( bmi < 10 )
+    else if ( bmi < 10 || bmi > 40 )
     {
-        document.getElementById("result").innerHTML = plump ;
+        document.getElementById("result").innerHTML = alien ;
     }
 
 }
