bf6d091edbc013da2a76f455163a42fd6fcb75ae
RainyF0rest <107770951+RainyF0rest@users.noreply.github.com>
2023-04-28 23:12:21 +0800

    button updated

--- BMICal.js
+++ BMICal.js
@@ -1,15 +1,16 @@
 function bmiCal()
 {
     //declaration 
-    w = document.getElementById("w").value;
-    h = document.getElementById("h").value/100;
+    var w = document.getElementById("w").value;
+    var h = document.getElementById("h").value/100;
     //result declaration 
-    thin = '<iframe src="https://giphy.com/embed/JKb28stpt0gwg" width="480" height="320" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p> <p >↑You are too thin , eat more↑</p></p>'
-    medium = '<iframe src="https://giphy.com/embed/bDL3BsB4ViI2k" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>↑You are healthy and fit ! ↑</p>'
-    chubby = '<iframe src="https://giphy.com/embed/47EstTgsR1vpK" width="480" height="369" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>↑Bit of chubby ! Still fine↑</p>'
-    plump = '<iframe src="https://giphy.com/embed/lN9amhr8GZMhG" width="480" height="419" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>↑Overweight ! Start your diet or you may suffer with health issue !↑</p>'
-    bmi = w/((h*h));
+    const thin = '<iframe src="https://giphy.com/embed/JKb28stpt0gwg" width="480" height="320" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p> <p >↑You are too thin , eat more↑</p></p>'
+    const medium = '<iframe src="https://giphy.com/embed/bDL3BsB4ViI2k" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>↑You are healthy and fit ! ↑</p>'
+    const chubby = '<iframe src="https://giphy.com/embed/47EstTgsR1vpK" width="480" height="369" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>↑Bit of chubby ! Still fine↑</p>'
+    const plump = '<iframe src="https://giphy.com/embed/lN9amhr8GZMhG" width="480" height="419" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p>↑Overweight ! Start your diet or you may suffer with health issue !↑</p>'
+    const bmi = w/((h*h));
     console.log(`BMI : ${bmi}`);
+    document.getElementById("BMI").value = bmi;
 
     if( bmi < 18.5)
     {
