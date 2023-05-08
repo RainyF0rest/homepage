76f4c0943c2676f48fbb6405654f44a67ddce853
RainyF0rest <107770951+RainyF0rest@users.noreply.github.com>
2023-05-08 23:13:23 +0800

    stopwatch test

---
+++ StopWatch.js
@@ -0,0 +1,9 @@
+
+    var appendH = document.getElementById("reset") ;
+    var appendW = document.getElementById("hr") ;
+
+    appendH.onclick = function test() 
+    {
+        appendW.innerHTML = "Hello World" ;
+    }
+
