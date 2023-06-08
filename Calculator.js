b2b3b2835eb5dcfd0232fb30531b22f5ed2e1389
RainyF0rest <107770951+RainyF0rest@users.noreply.github.com>
2023-06-09 00:22:02 +0800

    FixxedAllBug WorkingCSS

--- Calculator.js
+++ Calculator.js
@@ -2,50 +2,97 @@ window.onload = function ()
 {
     var value = 0;
     var result = false;
-    const plus = "+";
-    const minus = "-";
-    const times = "*";
-    const divide = "/";
-    const dot = ".";
+    var numNode = document.querySelectorAll(".num");
+    var operatorNode = document.querySelectorAll(".operator");
 
-    const one = "1";
-    const two = "2";
-    const three = "3";
-    const four = "4";
-    const five = "5";
-    const six ="6";
-    const seven = "7";
-    const eight = "8";
-    const nine = "9";
-    const zero = "0";
-
-    const buttonOne = document.getElementById("one");
-    const buttonTwo = document.getElementById("two");
-    const buttonThree = document.getElementById("three");
-    const buttonFour = document.getElementById("four");
-    const buttonFive = document.getElementById("five");
-    
-    const buttonSix = document.getElementById("six");
-    const buttonSeven = document.getElementById("seven");
-    const buttonEight = document.getElementById("eight"); 
-    const buttonNine = document.getElementById("nine");
-    const buttonZero = document.getElementById("zero");
-
-    const buttonPlus = document.getElementById("plus");
-    const buttonMinus = document.getElementById("minus");
-    const buttonTimes = document.getElementById("multiply");
-    const buttonDivide = document.getElementById("divide");
-
-    const buttonFloat = document.getElementById("float");
     const buttonEqual = document.getElementById("equal");
     const displayValue = document.getElementById("display");
     const buttonClear = document.getElementById("clear");
+    const buttonFloat = document.getElementById("float");
+
+    function numPadSimilator()
+    {
+        
+        for (var i = 0, n = numNode.length; i < n; i ++)
+        {
+            numNode[i].addEventListener("click", function append(e)
+    {
+        resultChecker();
+        value += e.target.innerHTML;
+        show();
+    })
+        }
+    }
+
+    function operatorSimulator()
+    {
+        for (var i = 0, n = operatorNode.length; i < n; i++ )
+        {
+            operatorNode[i].addEventListener("click", function appendd(e)
+            {
+                function oneOperatorOnce()
+                {
+                        if (value[value.length-1] === "+" || value[value.length-1] === "-" || value[value.length-1] === "x" || value[value.length-1] === "÷")
+                        {
+                            var newValue = value.substring(0,value.length - 1) + e.target.innerHTML;
+                            value = newValue;
+                        }
+                        else
+                        {
+                            value += e.target.innerHTML; 
+                        }
+                }
+                if (result == true)
+                {
+                    result = false;
+                }
+
+
+                oneOperatorOnce();
+                show();
+            }
+)
+        }
+    }
+
+
+
+
+    function oneDotOnly()
+    {   
+        var countDot = 0 ;
+        for (var i = 0 , n = value.length; i < n ; i++)
+        {
+            if (value[i] === ".")
+            {
+                countDot +=1;
+            }
+            if (value[i] === "+" || value[i] === "-" || value[i] === "x" || value[i] === "÷")
+            {
+                if (countDot > 0)
+                {
+                    countDot -= 1 ;
+                }
+            }
+
+        }
+
+        if (countDot >0 )
+        {
+            return false;
+        }
+        else 
+        {
+            return true;
+        }
+    }
 
     function resultChecker()
     {
-        if ( result === "true")
+        if ( result === true)
         {
-            
+            value = "";
+            result = false;
         }
     }
 
@@ -59,7 +106,29 @@ window.onload = function ()
 
     function show()
     {
+        displayModi();
         displayValue.value = value ;
+    }
+
+    buttonFloat.onclick = function float()
+    {
+        function lastChar()
+        {
+            if (value[value.length - 1] == "+" || value[value.length - 1] == "-" || value[value.length - 1] == "/" || value[value.length - 1] == "*") 
+                {
+                    return false;
+                }
+            else 
+            {
+                return true;
+            }
+        }
+
+        if (oneDotOnly() == true && lastChar() == true )
+        {
+            value += buttonFloat.innerHTML;
+        }
+        show();
     }
 
     buttonClear.onclick = function clear()
@@ -68,132 +137,25 @@ window.onload = function ()
         show();
     }
 
-    buttonOne.onclick = function appendOne()
-    {
-        displayModi();
-        value += one;
-        console.log(value);
-        show();
-    }
-
-    buttonTwo.onclick = function appendTwo()
-    {
-        displayModi();
-        value += two;
-        console.log(value);
-        show();
-    }
-
-    buttonThree.onclick = function appendThree()
-    {
-        displayModi();
-        value += three;
-        console.log(value);
-        show();
-    }
-
-
-    buttonFour.onclick = function appendFour()
-    {
-        displayModi();
-        value += four;
-        console.log(value);
-        show();
-    }
-
-
-    buttonFive.onclick = function appendFive()
-    {
-        displayModi();
-        value += five;
-        console.log(value);
-        show();
-    }
-    
-    buttonSix.onclick = function appendSix()
-    {
-        displayModi();
-        value += six;
-        console.log(value);
-        show();
-    }
-        
-    buttonSeven.onclick = function appendSeven()
-    {
-        displayModi();
-        value += seven;
-        console.log(value);
-        show();
-    }
-
-            
-    buttonEight.onclick = function appendEight()
-    {
-        displayModi();
-        value += eight;
-        console.log(value);
-        show();
-    }
-
-            
-    buttonNine.onclick = function appendNine()
-    {
-        displayModi();
-        value += nine;
-        console.log(value);
-        show();
-    }
-
-    buttonZero.onclick = function appendZero()
-    {
-        displayModi();
-        value += zero;
-        console.log(value);
-        show();
-    }
-
-    buttonPlus.onclick = function appendPlus()
-    {
-        displayModi();
-        value += plus;
-        console.log(value);
-        show();
-    }
-
-    buttonMinus.onclick = function appendMinus()
-    {
-        displayModi();
-        value += minus;
-        console.log(value);
-        show();
-    }
-
-    buttonTimes.onclick = function appendTimes()
-    {
-        displayModi();
-        value += times;
-        console.log(value);
-        show();
-    }
-
-    buttonDivide.onclick = function appendDivide()
-    {
-        displayModi();
-        value += divide;
-        console.log(value);
-        show();
-    }
-
-    buttonFloat.onclick = function appendFloat()
-    {
-        displayModi();
-        value += dot;
-        console.log(value);
-        show();
-    }
-
     buttonEqual.onclick = function calculate()
     {
+        function tranferer()
+        {
+            for (i = 0, n = value.length ; i < n ; i++)
+            {
+                if( value[i] == "x" )
+                {
+                    var newValue = value.substring(0, i) + "*" + value.substring(i + 1 , value.length);
+                    value = newValue;
+                }
+                if (value[i]== "÷" )
+                {
+                    var newValue = value.substring(0, i) + "/" + value.substring(i + 1 , value.length);
+                    value = newValue;
+                }
+            }
+        }
+        tranferer();
         ans = eval(value);
         value = ans ;
         result = true ;
@@ -201,4 +163,6 @@ window.onload = function ()
     }
 
     show();
+    numPadSimilator();
+    operatorSimulator();
 }
